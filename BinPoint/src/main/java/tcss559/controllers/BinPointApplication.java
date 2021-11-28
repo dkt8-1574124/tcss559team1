3eb4a3d68be24fd8d538a592b5a8ac81e678e7ae
dkt8-1574124 <dkt8@uw.edu>
2021-11-28 11:32:19 -0800

    First commit

---
+++ BinPoint/src/main/java/tcss559/controllers/BinPointApplication.java
@@ -0,0 +1,21 @@
+package tcss559.controllers;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+@ApplicationPath("/api")
+// This main java class will be used to declare a root 
+// resource for our application as well as other
+// provider classes
+public class BinPointApplication extends Application{
+	// This method returns a collection (non-empty) with
+	// specific classes to provide support for which are 
+	// going to be handled when published our JAX-RS application
+	@Override
+	public Set<Class<?>> getClasses() {
+		HashSet h = new HashSet<Class<?>>();
+		//add classes that you wish to be supported by application
+		//h.add( gcpREST.class );
+		return h;
+	}
+}
